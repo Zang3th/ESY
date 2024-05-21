7cfe5e50332172794027c722bde8aa4513f55253
Zang3th <tim.krueger1805@googlemail.com>
2024-05-21 16:21:22 +0200

    Cross compiling is working

--- Crosscompile/exports.sh
+++ Crosscompile/exports.sh
@@ -1,8 +1,9 @@
 echo "Preparing environment for this shell"
-HOMEDIR=$(echo ~)
+HOMEDIR=`echo ~`
 
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
-export SYSROOT=~/Dev/ESY/buildroot/output/host/usr/arm-buildroot-linux-uclibcgnueabihf
-export PATH=$PATH:~/Dev/ESY/buildroot/output/host/usr/bin/
+export SYSROOT=${HOMEDIR}/Dev/ESY/buildroot/output/host/usr/arm-buildroot-linux-uclibcgnueabihf
+export PATH="${PATH}:${HOMEDIR}:/Dev/ESY/buildroot/output/host/usr/bin/"
+export KDIR=${HOMEDIR}/Dev/ESY/Kernel/linux_server/linux/
 export KERNEL=kernel7l
